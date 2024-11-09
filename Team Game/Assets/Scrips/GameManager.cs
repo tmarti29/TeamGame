3a171f701b6861133445e1e26cde45198010905b
zbauerFSU <zbauer@student.fitchburgstate.edu>
2024-11-09 14:48:20 -0500

    Added SpawnBalls() and StartGame() Methods
    CAUSES ERROR RELATED TO LINE 27
    I cannot figure out what it is exactly

--- Team Game/Assets/Scrips/GameManager.cs
+++ Team Game/Assets/Scrips/GameManager.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> Balls;
+    private float spawnRate = 2.0f;
+    public bool isGameActive;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void StartGame()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnBalls());
+    }
+    IEnumerator SpawnBalls()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(Balls);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
