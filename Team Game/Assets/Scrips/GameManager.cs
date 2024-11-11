4073128fba437d3391a4f7b581eb03ac3350daf3
tmarti29 <tmarti29@student.fitchburgstate.edu>
2024-11-11 15:36:42 -0500

    Tried to fix ball spawning
    they still don't spawn, but it compiles now...

--- Team Game/Assets/Scrips/GameManager.cs
+++ Team Game/Assets/Scrips/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     public List<GameObject> Balls;
     private float spawnRate = 2.0f;
     public bool isGameActive;
-
+    public float ranX;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +24,9 @@ public class GameManager : MonoBehaviour
         while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
-            Instantiate(Balls);
+            ranX = Random.Range(-10f, 10f);
+            Vector3 spawnPos = new Vector3(ranX, 10, 0);
+            Instantiate(Balls[0], spawnPos, Quaternion.AngleAxis(0,Vector3.forward));
         }
     }
     // Update is called once per frame
